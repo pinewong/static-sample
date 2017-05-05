b1206325e7682210d5e3b63de6cb0038dbe6d40c
PineWong <pinewong@163.com>
2017-05-05 17:54:52 +0800

    Init

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+  $name = 'Default Name';
+  $user = 'Default User';
+  $saying = 'Default Saying';
+  $copy = '由 <a href="http://demoapp.cn">&copy;Demo APP 自动生成</a>，南大软院作品.';
+  $action = '<button class="pure-button pure-button-primary">关闭事例页</button>'
+
+  include_once('index.html')
+?>
